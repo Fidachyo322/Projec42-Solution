01761c462f2f4688e0b3024ca28ab78c3b113265
Fidachyo322 <baconvevo@mail.ru>
2025-03-23 19:33:56 +0300

    task 02

--- Projec42/logic.h
+++ Projec42/logic.h
@@ -1,2 +1,3 @@
 int get_extreme_index(int* array, int size, bool type);
 int calculate_product_between_extreme_elements(int* array, int size);
+void bubble_sort_asc(int* array, int size);
--- Projec42/main.cpp
+++ Projec42/main.cpp
@@ -7,16 +7,20 @@ int main() {
 	int size;
 
 	cout << "Imput size of vector: ";
-
 	cin >> size;
 
-	//random_init(array, size, -100, 100);
-	cout << "Imput vector elements:\n";
-	user_init(array, size);
-	cout << "Vector: " << convert(array, size) << endl;
+	random_init(array, size, -100, 100);
+	//cout << "Imput vector elements:\n";
+	//user_init(array, size);
+	cout << "Vector before: " << convert(array, size) << endl;
 
-	int result = calculate_product_between_extreme_elements(array, size);
-	cout << "Product of elements between exstreme value: " << result;
+	//task 01
+	//int result = calculate_product_between_extreme_elements(array, size);
+	//cout << "Product of elements between exstreme value: " << result;
+
+	//task 02
+	bubble_sort_asc(array, size);
+	cout << "Vector after: " << convert(array, size) << endl;
 
 	return 0;
 }
