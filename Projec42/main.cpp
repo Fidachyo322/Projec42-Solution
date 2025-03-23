c82361ff4da9641b5647f9c907dea8c65606ccd0
Fidachyo322 <baconvevo@mail.ru>
2025-03-23 18:47:15 +0300

    Add project files.

---
+++ Projec42/main.cpp
@@ -0,0 +1,15 @@
+#include "util.h"
+#define BUF 100
+int main() {
+
+	int array[BUF];
+	int size;
+
+	cout << "Imput size of vector: ";
+	cin >> size;
+
+	random_init(array, size, -100, 100);
+	cout << "Vector: " << convert(array, size) << endl;
+
+	return 0;
+}
